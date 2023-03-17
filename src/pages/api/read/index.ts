c56c333eea41ec87a786706a1fb7681502ddec40
fawaz-alesayi <fawaztsa@gmail.com>
2023-03-17 09:50:04 +0300

    implement wrapper for chatGPT vectara api

--- src/pages/api/read/index.ts
+++ src/pages/api/read/index.ts
@@ -1,74 +1,148 @@
 import {
   AzureKeyCredential,
   DocumentAnalysisClient,
-  DocumentSpan,
+  type DocumentSpan,
 } from "@azure/ai-form-recognizer";
-import { NextApiRequest, NextApiResponse } from "next";
-import { env } from "~/env.mjs";
+import { type NextApiRequest, type NextApiResponse } from "next";
 import { z } from "zod";
 import { writeFile } from "fs/promises";
 import { RecursiveCharacterTextSplitter } from "langchain/text_splitter";
 
-const urlSchema = z.object({
-  url: z.string()
-});
+const vectaraUrl = "https://experimental.willow.vectara.io/v1/chat/completions";
 
-// eslint-disable-next-line @typescript-eslint/require-await
-const handler = async (req: NextApiRequest, res: NextApiResponse) => {
-  const urlToRead = urlSchema.parse(req.body).url;
+const userPrompt =
+  "You are an investor, what are the most important stats in this report. Also please explain every technical term you use in Layman term";
 
-  const endpoint = "https://haseef-recognizer.cognitiveservices.azure.com/";
-  const credential = new AzureKeyCredential(process.env.AZURE_API_KEY);
-
-  const client = new DocumentAnalysisClient(endpoint, credential);
-
-  const poller = await client.beginAnalyzeDocumentFromUrl(
-    "prebuilt-document",
-    // The form recognizer service will access the following URL to a receipt image and extract data from it
-    urlToRead
-  );
-
-  // The "prebuilt-read" model (`beginReadDocument` method) only extracts information about the textual content of the
-  // document, such as page text elements and information about the language of the text.
-  const result = await poller.pollUntilDone();
-  const { pages, content, styles, languages } = result;
-
-  if (!pages || pages.length <= 0) {
-    res
-      .status(400)
-      .json({ error: "No pages were extracted from the document." });
-  } else {
-    // join all the paragraphs with \\n
-    if (!result.paragraphs) throw new Error("No paragraphs found");
-    const splitter = new RecursiveCharacterTextSplitter();
-
-    const p_content = result.paragraphs.map((p) => p.content);
-
-    const output = await splitter.createDocuments(p_content);
-
-    await writeFile(
-      `splitted-text.json`,
-      JSON.stringify(output),
-      "utf8"
-    );
-  }
-  await writeFile(`result.json`, JSON.stringify(result), "utf8");
-  res.status(200).json({ content, pages, languages, styles });
+type SystemMessage = {
+  role: "system";
+  content: string;
 };
 
-/**
- * Gets the text slices that a set of spans refer to from a document's content.
- *
- * @param content - the `content` field of an analyze result (reading-order concatenated text)
- * @param spans - an iterable of DocumentSpan objects that will be used to slice the content
- */
-export function* getTextOfSpans(
-  content: string,
-  spans: Iterable<DocumentSpan>
-): Iterable<string> {
-  for (const span of spans) {
-    yield content.slice(span.offset, span.offset + span.length);
+export const vectaraPayload = (
+  systemMessages: SystemMessage[],
+  userPrompt: string
+) => {
+  return {
+    model: "gpt-3.5-turbo",
+    messages: [
+      ...systemMessages,
+      {
+        role: "user",
+        content: userPrompt,
+      },
+    ],
+  };
+};
+
+export const requestSchema = z.discriminatedUnion("type", [
+  z.object({
+    type: z.literal("url"),
+    url: z.string().url(),
+  }),
+  z.object({
+    type: z.literal("text"),
+    text: z.string(),
+  }),
+]);
+
+const chatCompletionSchema = z.object({
+  id: z.string(),
+  object: z.literal("chat.completion"),
+  created: z.number(),
+  model: z.string(),
+  usage: z.object({
+    prompt_tokens: z.number(),
+    completion_tokens: z.number(),
+    total_tokens: z.number(),
+  }),
+  choices: z.array(
+    z.object({
+      message: z.object({
+        role: z.enum(["system", "assistant", "user"]),
+        content: z.string(),
+      }),
+      finish_reason: z.string(),
+      index: z.number(),
+    })
+  ),
+});
+
+const getChatCompletion = async (chunks: any) => {
+  const sys_messages = chunks.map((c) => {
+    return {
+      role: "system",
+      content: c.pageContent,
+    } as const;
+  });
+
+  const response = await fetch(vectaraUrl, {
+    method: "POST",
+    headers: {
+      "customer-id": "2421414240",
+      "x-api-key": process.env["VECTARA_API_KEY"],
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(vectaraPayload(sys_messages, userPrompt)),
+  });
+
+  return chatCompletionSchema.parse(await response.json());
+};
+
+const handler = async (req: NextApiRequest, res: NextApiResponse) => {
+  const payload = requestSchema.parse(req.body);
+
+  // Split the content into chunks able to consumed by ChatGPT
+  const splitter = new RecursiveCharacterTextSplitter();
+
+  if (payload.type == "url") {
+    const endpoint = "https://haseef-recognizer.cognitiveservices.azure.com/";
+    const credential = new AzureKeyCredential(process.env.AZURE_API_KEY);
+
+    const client = new DocumentAnalysisClient(endpoint, credential);
+
+    const poller = await client.beginAnalyzeDocumentFromUrl(
+      "prebuilt-document",
+      // The form recognizer service will access the following URL to a receipt image and extract data from it
+      payload.url
+    );
+
+    // The "prebuilt-read" model (`beginReadDocument` method) only extracts information about the textual content of the
+    // document, such as page text elements and information about the language of the text.
+    const result = await poller.pollUntilDone();
+    const { pages, content } = result;
+    let chatResponse: z.infer<typeof chatCompletionSchema>;
+
+    if (!pages || pages.length <= 0) {
+      res
+        .status(400)
+        .json({ error: "No pages were extracted from the document." });
+    } else {
+      // join all the paragraphs with \\n
+      if (!result.paragraphs) throw new Error("No paragraphs found");
+
+      const chunks = await splitter.createDocuments([content]);
+
+      chatResponse = await getChatCompletion(chunks);
+
+      await writeFile(`splitted-text.json`, JSON.stringify(chunks), "utf8");
+      await writeFile(
+        "chatgpt-response.json",
+        JSON.stringify(chatResponse),
+        "utf8"
+      );
+
+      res.status(200).json({
+        assitantResponse: chatResponse.choices[chatResponse.choices.length - 1],
+      });
+    }
+  } else if (payload.type === "text") {
+    const chunks = await splitter.createDocuments([payload.text]);
+    const chatResponse = await getChatCompletion(chunks);
+
+    res.status(200).json({
+      assitantResponse: chatResponse.choices[chatResponse.choices.length - 1],
+    });
   }
-}
+};
 
 export default handler;
