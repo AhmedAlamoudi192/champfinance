8a006865464cf55f7aab4f6a2b793978746e4cf5
Abdulaziz Alali <aoa200998@gmail.com>
2023-03-17 13:44:11 +0300

    more data

--- src/pages/api/trpc/test.ts
+++ src/pages/api/trpc/test.ts
@@ -4,7 +4,7 @@ import scrapeIt from "scrape-it";
 export default async function handler({ url }) {
   console.log("begin the request");
     let rows = [];
-    url = "https://www.saudiexchange.sa/wps/portal/saudiexchange/hidden/company-profile-main/!ut/p/z1/rZFNc5swEIZ_iw8cba3BxqQznY4gNqYFDKZxHS4egbFMAhLhw7L59VWSkzNu2kN1W-2zo2dfoRhtUczIKaekzTkjhawfY303xTqoSwNW9kKdQ7i4M8P7uT2GHxP06xqAKJhKIPA0F9Zgg47if5mHPxwMf5-PrxHDs3UIfRyu1NkUIFI_AjcU34DPHD61sGfoO4ppwZP3wDBLNIOiuM4OWZ3Vo66W18e2rZovCigghBhRzmmRjVJeKiDb316-Xvcb0u3z7JweCaOZrORt1ShQ8bolhQJXbQV4V5ekfs5aiZQkZ8P3aihImx5vGR1506LttcgtrslInR4rUpOy-bjCf1RUYNC1clSBfqwATHbR050VXJ6b5gLnwPWY18PJwwfniRt9_nOt-feOMFzcG-7mnDIMlpaINnCFt3PA671-LMbzjY3DZYmdjRlQMjzTuZ3QpFwVyzkJDhjvhW9ZTSAqPyfDWflgU6au6GlqT18eF2Zj2jrOzyvTOuHJhFiajx0qXdYhjhY8cl_Deo3jLY2W7Ino5M4oljlWhF2iS5lw-ekzFTQUZQxV5cMWcmcYJxetOLny_cHgN3wrSWI!/p0/IZ7_5A602H80O0VC4060O4GML81G57=CZ6_5A602H80OGF2E0QF9BQDEG10K4=NJstatementsTabData=/?statementType=2&reportType=0&symbol=7203"
+    url = "https://www.saudiexchange.sa/wps/portal/saudiexchange/hidden/company-profile-main/!ut/p/z1/rZFNc5swEIZ_iw8cba3BxqQznY4gNqYFDKZxHS4egbFMAhLhw7L59VWSkzNu2kN1W-2zo2dfoRhtUczIKaekzTkjhawfY303xTqoSwNW9kKdQ7i4M8P7uT2GHxP06xqAKJhKIPA0F9Zgg47if5mHPxwMf5-PrxHDs3UIfRyu1NkUIFI_AjcU34DPHD61sGfoO4ppwZP3wDBLNIOiuM4OWZ3Vo66W18e2rZovCigghBhRzmmRjVJeKiDb316-Xvcb0u3z7JweCaOZrORt1ShQ8bolhQJXbQV4V5ekfs5aiZQkZ8P3aihImx5vGR1506LttcgtrslInR4rUpOy-bjCf1RUYNC1clSBfqwATHbR050VXJ6b5gLnwPWY18PJwwfniRt9_nOt-feOMFzcG-7mnDIMlpaINnCFt3PA671-LMbzjY3DZYmdjRlQMjzTuZ3QpFwVyzkJDhjvhW9ZTSAqPyfDWflgU6au6GlqT18eF2Zj2jrOzyvTOuHJhFiajx0qXdYhjhY8cl_Deo3jLY2W7Ino5M4oljlWhF2iS5lw-ekzFTQUZQxV5cMWcmcYJxetOLny_cHgN3wrSWI!/p0/IZ7_5A602H80O0VC4060O4GML81G57=CZ6_5A602H80OGF2E0QF9BQDEG10K4=NJstatementsTabData=/?statementType=0&reportType=0&symbol=7203"
   const req1 = scrapeIt(url, { 
     titles: " thead.cf .numeric",
     values: "tbody .numeric",
@@ -48,7 +48,8 @@ export default async function handler({ url }) {
       "&" +
       data.titles.slice(30, 40);
 
-    data.titles = data.titles.split("&");
+      data.titles = data.titles.split("&");
+      
 
     let allowedLables = [
       "Current Assets",
@@ -69,7 +70,9 @@ export default async function handler({ url }) {
         allowedLables.splice(i, 1);
       }
     }
-    data.labels = allowedLables;
+      data.labels = allowedLables;
+    console.log(data.labels)
+
 
     let index = 0;
     data.labels.map((label) => {
@@ -84,10 +87,8 @@ export default async function handler({ url }) {
         "," +
         data.values[index + 3];
       index = index + 4;
-      console.log(row);
       rows.push(row);
     });
-    console.log(data);
   });
 
   let newUrl = url.replace("statementType=0", "statementType=2");
@@ -157,6 +158,98 @@ export default async function handler({ url }) {
         allowedLables.splice(i, 1);
       }
     }
+      data.labels = allowedLables;
+      console.log(data.labels)
+
+    let index = 0;
+    data.labels.map((label) => {
+      let row =
+        label +
+        "," +
+        data.values[index] +
+        "," +
+        data.values[index + 1] +
+        "," +
+        data.values[index + 2] +
+        "," +
+        data.values[index + 3];
+      index = index + 4;
+      rows.push(row); 
+    });
+   
+  });
+    
+    let newUrl2 = url.replace("statementType=0", "statementType=1");
+
+  const req3 = scrapeIt(newUrl2, {
+    titles: "thead.cf .numeric",
+    values: "tbody .numeric",
+    labels: "tbody tr td:first-child",
+  }).then(({ data, response }) => {
+    console.log(`Status Code: ${response.statusCode}`);
+
+    while (data.values.toString().includes("\t"))
+      data.values = data.values.toString().replace("\t", "");
+
+    while (data.values.toString().includes("\n"))
+      data.values = data.values.toString().replace("\n", "");
+
+    while (data.values.toString().includes("\r"))
+      data.values = data.values.toString().replace("\r", " ");
+
+    while (data.values.toString().includes("--"))
+      data.values = data.values.toString().replace("--", "   -   -");
+
+    while (data.values.toString().includes("   "))
+      data.values = data.values.toString().replace("   ", "&");
+
+    while (data.values.toString().includes("  "))
+      data.values = data.values.toString().replace("  ", "&");
+
+    while (data.values.toString().includes(","))
+      data.values = data.values.toString().replace(",", "");
+
+    data.values = data.values.split("&");
+
+    data.values = data.values.filter((value) => value != "");
+    data.values = data.values.map((value) => value.trim());
+
+    data.titles = data.titles.replace("Balance Sheet", "");
+    data.titles =
+      data.titles.slice(0, 10) +
+      "&" +
+      data.titles.slice(10, 20) +
+      "&" +
+      data.titles.slice(20, 30) +
+      "&" +
+      data.titles.slice(30, 40);
+
+    data.titles = data.titles.split("&");
+
+    let allowedLables = [
+      "Sales",
+      "Sales Cost", 
+      "Total Income",
+      "Other Revenues",
+      "Total Revenues",
+      "Admin and Marketing Expenses",
+      "Depreciation",
+      "Total Expenses",
+      "Net Income Before Zakat",
+      "Zakat",
+      "Net Income",
+      "Balance First Period",
+      "Reserves",
+      "Balance End Period",
+      "Cash Dividends",
+      "Other Expenses",
+      "Other Distributions", 
+    ];
+    for (let i = 0; i < allowedLables.length; i++) {
+      if (!data.labels.includes(allowedLables[i]?.toString())) {
+        allowedLables.splice(i, 1);
+      }
+    }
     data.labels = allowedLables;
 
     let index = 0;
@@ -177,8 +270,9 @@ export default async function handler({ url }) {
    
   });
 
-  return await Promise.all([req1, req2]).then(() => {
-     console.log(rows);
+  return await Promise.all([req1, req2,req3]).then(() => {
+     console.log(rows); 
     return rows;
   })
 }
+ 
